ecf9cbcc28cb88e6bc79dccd3148bd3a54bb3a14
Giles Dring <gilesdring@gmail.com>
2019-01-10 15:26:10 +0000

    Removing `.merged` addition to the filename

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash +x
 
 for FILE in /data/input/*.geojson; do
-  TARGET_FILE=/data/output/$(basename ${FILE} .geojson).merged.geojson
+  TARGET_FILE=/data/output/$(basename ${FILE})
   ogr2ogr input.shp ${FILE}
   ogr2ogr -f "GeoJSON" ${TARGET_FILE} input.shp \
       -dialect sqlite \
