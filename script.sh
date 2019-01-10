ee1eb50386bf8ea04033913ea07f1b58608dec26
Giles Dring <gilesdring@gmail.com>
2019-01-10 15:29:44 +0000

    Adding a progress message

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash +x
 
 for FILE in /data/input/*.geojson; do
+  echo Processing ${FILE}
   TARGET_FILE=/data/output/$(basename ${FILE})
   ogr2ogr input.shp ${FILE}
   ogr2ogr -f "GeoJSON" ${TARGET_FILE} input.shp \
